6d3d06614bff4e1f431aa23f2b14fb09c391b955
Dalton Claybrook <daltonclaybrook@gmail.com>
2021-07-06 19:08:28 -0400

    Cleanup for portrait mode

--- AstroSwift/Sources/SolarSystemView.swift
+++ AstroSwift/Sources/SolarSystemView.swift
@@ -29,9 +29,14 @@ struct SolarSystemView: View {
                 }
                 Image("sun")
             }
+            .position(x: geometry.size.width / 2.0, y: geometry.size.height / 2.0)
+        }
+        .background {
+            Image("background")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
         }
         .ignoresSafeArea()
-        .background(Image("background"))
         .onAppear {
             viewModel.startAdvancingTime(
                 startDate: Date(timeIntervalSince1970: 0),
@@ -43,8 +48,9 @@ struct SolarSystemView: View {
     // MARK: - Helpers
 
     private func getEllipseSize(with proxy: GeometryProxy, for planet: Planet) -> CGSize {
-        let maxWidth = proxy.size.width - 80.0
-        let maxHeight = proxy.size.height - 40.0
+        let (widthPadding, heightPadding) = proxy.size.width > proxy.size.height ? (80.0, 40.0) : (40.0, 80.0)
+        let maxWidth = proxy.size.width - widthPadding
+        let maxHeight = proxy.size.height - heightPadding
         let multiplier = viewModel.ellipseSizeMultiplier(for: planet)
         return CGSize(
             width: maxWidth * multiplier,
@@ -56,6 +62,6 @@ struct SolarSystemView: View {
 struct SolarSystemView_Previews: PreviewProvider {
     static var previews: some View {
         SolarSystemView()
-            .previewInterfaceOrientation(.landscapeLeft)
+            .previewInterfaceOrientation(.portrait)
     }
 }
