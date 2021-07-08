0aafaf54b86b4b5ced42ba4b0073b6b88ed24db2
Dalton Claybrook <daltonclaybrook@gmail.com>
2021-07-08 09:37:27 -0400

    Update swift package to use hosted binary target

--- Package.swift
+++ Package.swift
@@ -8,11 +8,16 @@ let package = Package(
         .iOS(.v9)
     ],
     products: [
-        .library(name: "SpaceKit", targets: ["SpaceKit", "libspacekit"]),
+        .library(name: "SpaceKit", targets: ["SpaceKit"]),
     ],
     targets: [
-        .target(name: "SpaceKit", dependencies: []),
-        .binaryTarget(name: "libspacekit", path: "libspacekit.xcframework"),
+        .target(name: "SpaceKit", dependencies: ["libspacekit"]),
+        .binaryTarget(
+            name: "libspacekit",
+            url: "https://github.com/daltonclaybrook/SpaceKit/releases/download/v0.1.1/libspacekit.xcframework.zip",
+            checksum: "fa34c3792517c41cfbe695164a3c490671c9c028b2ef369a5ffcc2079e8a447b"
+        ),
+//        .binaryTarget(name: "libspacekit", path: "libspacekit.xcframework"),
         .testTarget(name: "SpaceKitTests", dependencies: ["SpaceKit"]),
     ]
 )
